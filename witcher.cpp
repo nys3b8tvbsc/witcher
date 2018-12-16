4e333f024ac6a5ea035bab68a252f124d67be358
nys3b8tvbsc <nys3b8tvbsc@yandex.ru>
2018-12-17 00:26:28 +0300

    Add WeaponType enum /  oiling method improvement

--- witcher.cpp
+++ witcher.cpp
@@ -2,8 +2,8 @@
 
 using std::make_unique;
 
-Witcher::Witcher() {
-	
+Witcher::Witcher()
+{
 	hp = 200 + rand() % 201;
 	armor = 5 + rand() % 21;
 
@@ -14,6 +14,8 @@ Witcher::Witcher() {
 	fists = make_unique<Fists>(2 + rand() % 9);
 	steel_sword = make_unique<SteelSword>(50 + rand() % 21);
 	silver_sword = make_unique<SilverSword>(45 + rand() % 51);
+
+	active_weapon = nullptr;
 }
 
 void Witcher::take_damage(int damage)
@@ -23,9 +25,80 @@ void Witcher::take_damage(int damage)
 	this->hp = (this->hp > 0)? this->hp: 0;
 }
 
+void Witcher::attack(Enemy& enemy) const
+{
+	active_weapon->attack(enemy);	
+}
 
-//??
-void Witcher::attack(Enemy& enemy) const {
-	auto weapon = active_weapon.lock();
-	weapon->attack(enemy);
+void Witcher::set_fight_style(EnemyType id, Enemy* enemy)
+{
+	switch(id) {
+		case EnemyType::DRACONID:
+			fight_style = make_unique<DraconidFight>(this, enemy);
+			break;
+		case EnemyType::GHOST:
+			fight_style = make_unique<GhostFight>(this, enemy);
+			break;
+		case EnemyType::NECROPHAGE:
+			fight_style = make_unique<NecrophageFight>(this, enemy);
+			break;
+		case EnemyType::HUMAN:
+			fight_style = make_unique<HumanFight>(this, enemy);
+			break;
+		case EnemyType::ROGUE:
+			fight_style = make_unique<RogueFight>(this, enemy);
+			break;
+	}	
+}
+
+void Witcher::start_fight()
+{
+	this->fight_style->start_fight();
+}
+
+void Witcher::set_weapon(WeaponType type)
+{
+	switch(type) {
+		case WeaponType::FISTS:
+			active_weapon = fists.get();
+			break;
+		case WeaponType::STEELSWORD:
+			active_weapon = steel_sword.get();
+			break;
+		case WeaponType::SILVERSWORD:
+			active_weapon = silver_sword.get();
+			break;
+	}
+}
+
+void Witcher::oil_silver_sword(OilType type)
+{
+	switch(type) {
+		case OilType::DRACONID:
+			if (draconid_oil) {
+				draconid_oil = false;
+				this->silver_sword->oil(type);
+			}
+			else
+				this->silver_sword->oil(OilType::NONE);
+			break;
+
+		case OilType::GHOST:
+			if (ghost_oil) {
+				ghost_oil = false;
+				this->silver_sword->oil(type);
+			}
+			else
+				this->silver_sword->oil(OilType::NONE);
+			break;
+
+		case OilType::NECROPHAGE:
+			if (necrophage_oil) {
+				necrophage_oil = false;
+				this->silver_sword->oil(type);
+			}
+			else
+				this->silver_sword->oil(OilType::NONE);
+			break;
+	}
 }
--- witcher.h
+++ witcher.h
@@ -3,8 +3,18 @@
 #include "weapon.h"
 #include "enemy.h"
 #include <memory>
+#include "fight.h"
 
-class Witcher //??
+class Fight;
+
+enum class WeaponType 
+{
+	FISTS,
+	STEELSWORD,
+	SILVERSWORD
+};
+
+class Witcher
 {
 	int hp;
 	int armor;
@@ -17,11 +27,20 @@ class Witcher //??
 	std::unique_ptr<SteelSword> steel_sword;
 	std::unique_ptr<SilverSword> silver_sword;
 
-	std::weak_ptr<Weapon> active_weapon; //??
+	Weapon* active_weapon;
+	std::unique_ptr<Fight> fight_style;
 
 public:
 	Witcher();
-
 	void take_damage(int damage);
-	void attack(Enemy& enemy) const; //??
+	void attack(Enemy& enemy) const;
+	void set_fight_style(EnemyType id, Enemy* enemy);
+	void start_fight();
+	void set_weapon(WeaponType type);
+	void oil_silver_sword(OilType type);
+	
+	bool is_dead() const
+	{
+		return hp == 0;
+	}
 };
